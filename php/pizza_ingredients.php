a7e99dcf9b25d69018b89823408441db5f18a4c0
builder <builder@genexus.com>
2023-03-23 17:46:52 +0100

    Remove unused file

--- php/pizza_ingredients.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include("db.php");
-
-	$db = $conn;
-	$tableName = "pizza_ingredients";
-	$columns = ['pizza_id', 'ingredient_id'];
-	$fetchData = fetch_data($db, $tableName, $columns);
-
-?>
